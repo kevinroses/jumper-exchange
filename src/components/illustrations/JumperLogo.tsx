742b3d3275ff1dab2b0af6ba7de97a16d4da9db4
kentur <kevinpalaciosdev@gmail.com>
2025-01-14 15:45:45 -0500

    Update JumperLogo.tsx

--- src/components/illustrations/JumperLogo.tsx
+++ src/components/illustrations/JumperLogo.tsx
@@ -1,3 +1,14 @@
 export const JumperLogo = () => {
-    return <></>;
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="jumper-logo"
+        width="100%"
+        height="100%"
+        fill="none"
+      >
+      </svg>
+    </>
+  );
 };
