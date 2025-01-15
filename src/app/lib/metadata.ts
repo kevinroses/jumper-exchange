92d72bf92e51596c284126e96dd9b75c25204aab
kentur <kevinpalaciosdev@gmail.com>
2025-01-15 09:47:23 -0500

    Update metadata.ts

--- src/app/lib/metadata.ts
+++ src/app/lib/metadata.ts
@@ -1,4 +1,4 @@
-export const siteName = 'Jumper.Exchange';
-export const title = 'Jumper | Multi-Chain Bridging & Swapping';
+export const siteName = '.Exchange';
+export const title = 'Multi-Chain Bridging & Swapping';
 export const description =
-  'Jumper is Crypto’s Everything Exchange, enabling seamless swap and bridge transactions across 25 blockchains. By aggregating all of DeFi liquidity, Jumper ensures the best routes for any token transaction.';
+  'is Crypto’s Everything Exchange, enabling seamless swap and bridge transactions across 25 blockchains. By aggregating all of DeFi liquidity, ensures the best routes for any token transaction.';
