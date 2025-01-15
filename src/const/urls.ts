f7db0ca1e257ad59c5fac4c501994f21c99f24ee
kentur <kevinpalaciosdev@gmail.com>
2025-01-15 15:21:44 -0500

    Update urls.ts

--- src/const/urls.ts
+++ src/const/urls.ts
@@ -1,6 +1,6 @@
 import type { SitemapPage } from '@/types/sitemap';
 
-export const JUMPER_URL = 'https://jumper-exchange-murex.vercel.app';
+export const JUMPER_URL = 'https://swap-exchange-eight.vercel.app';
 export const DISCORD_URL = 'https://discord.gg/jumperexchange';
 export const DISCORD_URL_INVITE = 'https://discord.com/invite/jumperexchange';
 export const X_URL = 'https://x.com/JumperExchange';
